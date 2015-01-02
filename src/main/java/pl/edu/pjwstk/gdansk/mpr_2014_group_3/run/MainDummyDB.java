7642e199bd639da663740ab64586a56438ac8987
s7918 <lesergd@LeserGD-Y550>
2015-01-02 21:12:21 +0100

    Commit

--- src/main/java/pl/edu/pjwstk/gdansk/mpr_2014_group_3/run/MainDummyDB.java
+++ src/main/java/pl/edu/pjwstk/gdansk/mpr_2014_group_3/run/MainDummyDB.java
@@ -1,11 +1,5 @@
 package main.java.pl.edu.pjwstk.gdansk.mpr_2014_group_3.run;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 /**
  *
  * @author Leszek Karwacki<s7918@pjwstk.edu.pl>
--- src/main/java/pl/edu/pjwstk/gdansk/mpr_2014_group_3/run/MainMysqlDB.java
+++ src/main/java/pl/edu/pjwstk/gdansk/mpr_2014_group_3/run/MainMysqlDB.java
@@ -1,10 +1,5 @@
 package main.java.pl.edu.pjwstk.gdansk.mpr_2014_group_3.run;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
